a4718130440dee2bec398f9ec36021ae801a1cbd
bismarkboateng <bismarkb609@gmail.com>
2023-01-06 00:25:00 +0000

    tasks

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+a type-annotated function add that takes a float
+'a' and a float 'b' as arguments and returns
+their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    '''
+    params: a, b - floats
+    return: float
+    '''
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+'''
+a type-annotated function concat that takes
+a string 'str1' and a string 'str2' as arguments
+and returns a concatenated string
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+
+    '''
+    :params: str1, str2 - str
+    return: str
+    '''
+    return str1 + str2
