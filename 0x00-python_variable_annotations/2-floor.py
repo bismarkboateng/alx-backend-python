f46ceac007ed8ed3272a13876f9b5803087cdff6
bismarkboateng <bismarkb609@gmail.com>
2023-01-06 00:34:20 +0000

    tasks

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import math
+
+'''
+a type-annotated function 'loor
+which takes a float 'n' as argument
+and returns the floor of the float.
+'''
+
+
+def floor(n: float) -> int:
+    '''
+    :params: n-float
+    return: int
+    '''
+    return math.floor(n)
